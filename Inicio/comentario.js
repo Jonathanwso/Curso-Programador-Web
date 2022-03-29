b2911e5608bc7d4b11b1b90b1a3f68544a972a51
Jonathan Oliveira <jonathanwdsoliveira@recife.pe.senac.br>
2022-03-29 12:13:19 -0300

    atualizados

---
+++ Inicio/comentario.js
@@ -0,0 +1,9 @@
+//console.log é usado para exibir algo na tela do computador!
+console.log("Texto");
+console.log('Texto 2'); // mais um exemplo de console.log!
+//console.log("Texto");
+
+/*
+Esse é um comentario de multiplas linhas!
+ */
+console.log("Texto");
